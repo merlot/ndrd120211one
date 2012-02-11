642fee6f895c764a9141cb88ba236932a237aaa7
Java Greenhorn <javax@mail.com>
2012-02-11 14:06:29 +0200

    First commit.

---
+++ LeoOne/src/com/livj/leoone/StartingPointActivity.java
@@ -0,0 +1,13 @@
+package com.livj.leoone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartingPointActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
